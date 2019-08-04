b53e698c5a16e59e79fa07609cb882685a80e139
Gabriel Arjones <arjones@arjones.com>
2019-08-03 22:12:30 -0300

    Remove unused code

--- src/extension.ts
+++ src/extension.ts
@@ -2,20 +2,11 @@
 // The module 'vscode' contains the VS Code extensibility API
 // Import the module and reference it with the alias vscode in your code below
 import * as vscode from "vscode";
-import * as debug from "./debug";
 
 // this method is called when your extension is activated
 // your extension is activated the very first time the command is executed
 export function activate(extensionContext: vscode.ExtensionContext) {
-    extensionContext.subscriptions.push(vscode.debug.onDidTerminateDebugSession((session: vscode.DebugSession) => {
-        // Kill all nodes
-    }));
-
-    const roslaunchDebugProvider = new debug.RoslaunchConfigurationProvider();
-    extensionContext.subscriptions.push(
-        vscode.debug.registerDebugConfigurationProvider(
-            "roslaunch",
-            roslaunchDebugProvider));
+  // Extension loaded
 }
 
 // this method is called when your extension is deactivated
