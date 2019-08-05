7adc96e7c3f2b688c36d4c51431cb8bd3889db06
Gabriel Arjones <arjones@arjones.com>
2019-08-05 11:34:14 -0300

    Add debugger stub (closes #1)

--- src/debug.ts
+++ src/debug.ts
@@ -16,24 +16,38 @@ export class RoslaunchConfigurationProvider implements DebugConfigurationProvide
         };
 
         const env = await new EnvLoader(folder!.uri.path).env();
-        const launchFile: string = this.expandVariables((config as any).launchFile, expandableVars);
+        const launchFile: string = this.expandVariables(config.launchFile, expandableVars);
         const launcher = path.join(__dirname, "..", "..", "helpers", "launch.py");
-        const node: string = (config as any).node;
-        const nodeArgsLoader = new NodeArguments(env, launchFile, node);
+        const nodeArgsLoader = new NodeArguments(env, launchFile, config.node);
 
         try {
             const nodeArgs = await nodeArgsLoader.args();
 
             if (nodeArgs.error) { throw new Error(nodeArgs.error); }
+            if (!config.MIMode) { config.MIMode = "gdb"; }
+            if (!config.environment) { config.environment = []; }
 
-            (config as any).type = "cppdbg";
-            (config as any).program = nodeArgs.args[0];
-            (config as any).args = nodeArgs.args.slice(1);
-            (config as any).env = nodeArgs.env;
-            (config as any).roslaunch = {
-                cmd: `"${launcher}" "${launchFile}" "${node}"`,
+            const debuggerPath = config.miDebuggerPath || config.MIMode;
+            config.miDebuggerPath = path.join(__dirname, "..", "..", "helpers", "debugger");
+            config.environment = config.environment.concat([
+                { name: "VSCODE_ROS_DEBUG_DEBUGGER", value: debuggerPath },
+            ]);
+
+            for (const key in nodeArgs.env) {
+                if (!nodeArgs.env.hasOwnProperty(key)) { continue; }
+                config.environment = config.environment.concat([
+                    { name: key, value: nodeArgs.env[key] },
+                ]);
+            }
+
+            config.type = "cppdbg";
+            config.program = nodeArgs.args[0];
+            config.args = nodeArgs.args.slice(1);
+            config.roslaunch = {
+                cmd: `"${launcher}" "${launchFile}" "${config.node}"`,
                 env,
             };
+
             return config;
         } catch (e) {
             const errMsg = nodeArgsLoader.stderr();
